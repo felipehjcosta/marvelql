9e332530194bcdaa763fd791f5f4960a2a20fce3
felipehjcosta <felipe.h.j.costa@gmail.com>
2019-01-06 15:00:14 -0200

    Add an integration test configuration

---
+++ integrationTest/com/github/felipehjcosta/ExampleTest.kt
@@ -0,0 +1,12 @@
+package com.github.felipehjcosta
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class ExampleTest {
+
+    @Test
+    internal fun example() {
+        assertEquals(4, 2 + 2)
+    }
+}
