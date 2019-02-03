481549ba82071d5764c7445de2b95f4d66f2e6fe
felipehjcosta <felipe.h.j.costa@gmail.com>
2019-02-02 23:04:07 -0200

    Use koin to organize dependencies

--- src/com/github/felipehjcosta/Application.kt
+++ src/com/github/felipehjcosta/Application.kt
@@ -4,8 +4,10 @@ package com.github.felipehjcosta
 
 import com.github.felipehjcosta.adapters.infrastructure.RemoteCharactersRepository
 import com.github.felipehjcosta.application.QueryCharactersService
+import com.github.felipehjcosta.domain.CharactersRepository
 import com.github.felipehjcosta.domain.MarvelCharacter
 import com.github.pgutkowski.kgraphql.KGraphQL
+import com.github.pgutkowski.kgraphql.schema.Schema
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -21,25 +23,14 @@ import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
 import kotlinx.coroutines.runBlocking
+import org.koin.ktor.ext.inject
+import org.koin.ktor.ext.installKoin
 
 const val BASE_URL = "https://gateway.marvel.com:443"
 val PUBLIC_KEY = System.getenv("MARVEL_PUBLIC_KEY")
 val PRIVATE_KEY = System.getenv("MARVEL_PRIVATE_KEY")
 
 fun Application.module() {
-    val schema = KGraphQL.schema {
-        query("characters") {
-            resolver<List<MarvelCharacter>> {
-                val response = runBlocking {
-                    QueryCharactersService(RemoteCharactersRepository(BASE_URL, PUBLIC_KEY, PRIVATE_KEY)).execute()
-                }
-                println(">>> response: ${response.characters}")
-                response.characters
-            }
-        }
-
-        type<MarvelCharacter>()
-    }
     install(ContentNegotiation) {
         gson()
     }
@@ -48,6 +39,29 @@ fun Application.module() {
             call.respond(HttpStatusCode.InternalServerError)
         }
     }
+    val module = org.koin.dsl.module {
+        single<CharactersRepository> { RemoteCharactersRepository(BASE_URL, PUBLIC_KEY, PRIVATE_KEY) }
+        single { QueryCharactersService(get()) }
+        single {
+            KGraphQL.schema {
+                query("characters") {
+                    resolver<List<MarvelCharacter>> {
+                        val response = runBlocking {
+                            get<QueryCharactersService>().execute()
+                        }
+                        println(">>> response: ${response.characters}")
+                        response.characters
+                    }
+                }
+
+                type<MarvelCharacter>()
+            }
+        }
+    }
+    installKoin {
+        modules(module)
+    }
+    val schema: Schema by inject()
     routing {
         post("/graphql") {
             val query = call.receiveText()
